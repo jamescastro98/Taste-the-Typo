ff073c10f8b474621de760455ce155b9839e442d
jamescastro98 <jamescastro98@gmail.com>
2019-10-23 12:18:47 -0400

    Added the code for the pyppeteer portion. We will have multiple processes execute this, with different strings being passed for argv[1], where string=website url

---
+++ webbrowse.py
@@ -0,0 +1,19 @@
+import sys
+import asyncio
+from pyppeteer import launch
+
+#if it don't work, check your wifi
+
+async def main():
+    browser = await launch()
+    page = await browser.newPage()
+    await page.goto(sys.argv[1]) #change this to website passed in.
+    await page.screenshot({'path': 'example.png'})
+    str= await page.content()
+    await browser.close()
+    f = open("browserdump.html", "w") #need to make an array of files to dump them at.
+    f.write(str)
+    print("Contents written in browserdump")
+    f.close()
+
+asyncio.get_event_loop().run_until_complete(main())
