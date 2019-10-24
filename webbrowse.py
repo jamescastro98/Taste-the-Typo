3e1b502a613a64a06e6d4f66a2e302217dc01112
jamescastro98 <jamescastro98@gmail.com>
2019-10-24 14:39:42 -0400

    I put it in a try/catch

--- webbrowse.py
+++ webbrowse.py
@@ -1,19 +1,19 @@
 import sys
 import asyncio
 from pyppeteer import launch
-
-#if it don't work, check your wifi
-
 async def main():
     browser = await launch()
     page = await browser.newPage()
-    await page.goto(sys.argv[1]) #change this to website passed in.
-    await page.screenshot({'path': 'example.png','fullPage' : True})
-    str= await page.content()
+    try:
+        await page.goto(sys.argv[1]) #change this to website passed in.
+        await page.screenshot({'path': 'example.png','fullPage' : True})
+        str= await page.content()
+    except:
+        str="Error Occurred - Either the website does not exist, or this device is not connected to wifi"
+        print(str)
     await browser.close()
     f = open("browserdump.html", "w") #need to make an array of files to dump them at.
     f.write(str)
-    print("Contents written in browserdump")
+    print("Contents written in browserdump (Error String stored in HTML doc if !exist)")
     f.close()
-
 asyncio.get_event_loop().run_until_complete(main())
