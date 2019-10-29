863dea1cbaa4d8c64fc27b05521a0ee3861a65d4
Joseph Spivack <joey.spivack@gmail.com>
2019-10-29 19:42:30 -0400

    small edit to the README

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This is the repository for the CSE 331 final project. We will be writing this modularly, meaning the web dashboard will execute the typogenerator,
 then after the list is generated, it will execute many instances of webbrowse.py (numinstances based upon user Input to dashboard)
 
-**Python Libraries**
+**Python Third Party Libraries**
 
 Pyppeteer - Python port of Puppeteer - used for creating headless chrome browsers.
 
--- typoGenerator.py
+++ typoGenerator.py
@@ -1,54 +1,58 @@
 import sys
 import json
 
-if len(sys.argv) < 2:
-    print("missing webiste")
-    sys.exit(0)
 
-addr = sys.argv[1]
-typos = []
+def generateTypos(site: str):
+    if len(sys.argv) < 2:
+        print("missing webiste")
+        sys.exit(0)
 
-# 1) Missing-dot typo
-if (addr.startswith("www.")):
-    typos.append(addr.replace(".","", 1))
-else:
-    typos.append("www" + addr)
-    addr = "www." + addr # if the webiste was entered without www. I add it
+    addr = site
+    typos = []
 
-# 2) Character-omission typos
-addrName = addr.split(".")[1]
-addrTopDomain = addr.split(".")[2]
-for i in range(0,len(addrName)):
-    typos.append("www." + addrName[:i] + addrName[i+1:] + "." + addrTopDomain)
+    # 1) Missing-dot typo
+    if (addr.startswith("www.")):
+        typos.append(addr.replace(".","", 1))
+    else:
+        typos.append("www" + addr)
+        addr = "www." + addr # if the webiste was entered without www. I add it
 
-# 3) Character-permutation typos
-    for i in range(0,len(addrName) - 1):
-        if (addrName[i] == addrName[i + 1]):
-            continue
-        typos.append("www." + addrName[:i] + addrName[i+1] + addrName[i] + addrName[i+2:] + "." + addrTopDomain)
+    # 2) Character-omission typos
+    addrName = addr.split(".")[1]
+    addrTopDomain = addr.split(".")[2]
+    for i in range(0,len(addrName)):
+        typos.append("www." + addrName[:i] + addrName[i+1:] + "." + addrTopDomain)
 
-# 4) Character-replacement typos
-# adjacency dictonary
-adjacencyFile = open("adjacency.json")
-adjacencyDict = json.load(adjacencyFile)
+    # 3) Character-permutation typos
+        for i in range(0,len(addrName) - 1):
+            if (addrName[i] == addrName[i + 1]):
+                continue
+            typos.append("www." + addrName[:i] + addrName[i+1] + addrName[i] + addrName[i+2:] + "." + addrTopDomain)
 
-for i in range(len(addrName)):
-    for j in adjacencyDict[addrName[i]]:
-        typos.append("www." + addrName[:i] + j + addrName[i+1:] + "." + addrTopDomain)
+    # 4) Character-replacement typos
+    # adjacency dictonary
+    adjacencyFile = open("adjacency.json")
+    adjacencyDict = json.load(adjacencyFile)
 
-# 5) Character-insertion typos
-# the definition for this one was slightly unclear might need to be adjusted
+    for i in range(len(addrName)):
+        for j in adjacencyDict[addrName[i]]:
+            typos.append("www." + addrName[:i] + j + addrName[i+1:] + "." + addrTopDomain)
 
-for i in range(len(addrName)):
-    charsadded = [];
-    typos.append("www." + addrName[:i+1] + addrName[i] + addrName[i+1:] + "." + addrTopDomain)
-    for j in adjacencyDict[addrName[i]]:
-        charsadded.append(j)
-        typos.append("www." + addrName[:i+1] + j + addrName[i+1:] + "." + addrTopDomain)
-    if i+1 < len(addrName):
-        for j in adjacencyDict[addrName[i+1]]:
-            if not j in charsadded:  
-                typos.append("www." + addrName[:i+1] + j + addrName[i+1:] + "." + addrTopDomain)
+    # 5) Character-insertion typos
+    # the definition for this one was slightly unclear might need to be adjusted
 
-adjacencyFile.close()
-print(typos)
+    for i in range(len(addrName)):
+        charsadded = [];
+        typos.append("www." + addrName[:i+1] + addrName[i] + addrName[i+1:] + "." + addrTopDomain)
+        for j in adjacencyDict[addrName[i]]:
+            charsadded.append(j)
+            typos.append("www." + addrName[:i+1] + j + addrName[i+1:] + "." + addrTopDomain)
+        if i+1 < len(addrName):
+            for j in adjacencyDict[addrName[i+1]]:
+                if not j in charsadded:  
+                    typos.append("www." + addrName[:i+1] + j + addrName[i+1:] + "." + addrTopDomain)
+
+    adjacencyFile.close()
+    return typos
+
+print(generateTypos(sys.argv[1]))
