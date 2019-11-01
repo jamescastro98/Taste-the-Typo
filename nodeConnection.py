8acad48ec237480d3294c81c069473b8fb1f5982
natchan29 <natchan@cs.stonybrook.edu>
2019-11-01 14:42:28 -0400

    expanded nodeConnection for testing

--- nodeConnection.py
+++ nodeConnection.py
@@ -2,11 +2,23 @@ from typoGenerator import generateTypos
 from webbrowse import test
 import sys
 import _thread
+import string
 
-typos = generateTypos('https://amazon.com')
-print(typos)
+typos = generateTypos('messenger.com') # switch to sysargv for custom input
+                                            # valid input: "https://amazon.com"
+# print(typos)
 for typo in typos:
-    try:
-        _thread.start_new_thread(test(typo))
-    except:
-        print('Error: unable to start thread')
+    msg = typo
+    if typo.find("www.", 0, 4) != -1:
+        msg = msg + ' = [valid]'   # debugging statement
+        typo = typo.replace("www.","")
+        if typo.find("https://", 0, 8)==-1:
+            typo = 'https://'+typo
+        # probably check if domain is also present
+        print('Prepped '+typo+' to enter test()')   # debugging statement
+        try:
+            _thread.start_new_thread(test(typo))
+        except:
+            print('Error: unable to start thread')
+    print(msg)  # debugging
+    
