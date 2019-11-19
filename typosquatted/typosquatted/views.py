40817e70b141f7035bb7b4ce1ed1ce1cb7438a07
James Castro <jamescastro98@gmail.com>
2019-11-19 14:37:19 -0500

    Basic appearance changes. Sleep views.py 5 seconds to mimic a request.

--- typosquatted/typosquatted/views.py
+++ typosquatted/typosquatted/views.py
@@ -1,7 +1,7 @@
 from django.views import generic
 from .forms import WebForm
 from django.shortcuts import render
-
+import time
 #from models import Post
 
 #change!
@@ -16,5 +16,8 @@ def ResultView(request):
     if request.method =='POST':
         form = WebForm(request.POST)
         if form.is_valid():
+            print("Request Gotten!")
+            #Execute Master + Worker Nodes Here
+            time.sleep(5)
             input = form.data["weburl"]
             return render(request, "result.html", {'input':input})
