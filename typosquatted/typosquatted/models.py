3b30b50396393151299a0778a3c2a40cf4a67395
Joseph Spivack <joey.spivack@gmail.com>
2019-11-30 19:21:15 -0500

    added more fields to the DB

--- typosquatted/typosquatted/models.py
+++ typosquatted/typosquatted/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class webData(models.Model):
-    name=models.CharField(max_length=150) #name of website - found in head of html doc as <title>NAME</title>
+    #name=models.CharField(max_length=150) #name of website - found in head of html doc as <title>NAME</title>
     origin=models.CharField(max_length=150) #original url (http://google.com , etc)
     typo=models.CharField(max_length=150) #typo'd urls
+    html=models.FileField(upload_to='data/{}/{}.html'.format(origin,typo))
+    img=models.FileField(upload_to='data/{}/{}.png'.format(origin,typo))
+
