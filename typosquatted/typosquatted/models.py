b17c5c0a9e5cc4c8bda04ae38d2a3cba9bd46487
Joseph Spivack <joey.spivack@gmail.com>
2019-11-30 19:43:29 -0500

    added primary key to model

--- typosquatted/typosquatted/models.py
+++ typosquatted/typosquatted/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class webData(models.Model):
     #name=models.CharField(max_length=150) #name of website - found in head of html doc as <title>NAME</title>
+    originandtypo=models.CharField(max_length=300, primary_key = True)
     origin=models.CharField(max_length=150) #original url (http://google.com , etc)
     typo=models.CharField(max_length=150) #typo'd urls
     html=models.FileField(upload_to='data/{}/{}.html'.format(origin,typo))
